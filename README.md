31dc2f8a18649e63d8207bb8cf16de75cab137a4
cartereric <eric.eugene.carter@gmail.com>
2021-04-14 20:25:30 -0400

    Remove carter-website references

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # root-config
+
 The root config for the application was created with the npm init single-spa command. It uses the single-spa-router.
 
-npm start
+## Running within svelte-react-vue-angular-spa
+
+Run `npm start`
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const singleSpaDefaults = require("webpack-config-single-spa");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "carter-website";
+  const orgName = "svelte-react-vue-angular-spa";
   const defaultConfig = singleSpaDefaults({
     orgName,
     projectName: "root-config",
